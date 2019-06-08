6daa4fd203c2c3e7b4941c81328968be127cd208
Cretrok <solosemeocurioesto@gmail.com>
2019-06-08 12:45:26 -0500

    Js

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,10 +1,18 @@
-var casillas = document.querySelectorAll("main div")
+var casillas = document.querySelectorAll("main div");
+var turno = "X";
 
 casillas.forEach(casilla => {
-    casilla.addEventListener("click", function(evento){
-    casilla.style = 
-    }
-})
+    casilla.addEventListener( "click", function (evento) {
+    	if( turno == 'X'){
+    		this.innerHTML = `<span class="movimiento croos">${turno}</span>`;
+    		turno = "O";
+    		console.log("click en: "+casilla);
+    	}else{
+    		this.innerHTML = `<span class="movimiento circle">${turno}</span>`
+    		turno = "X";
+    	}
+    });
+});    
 
 
 
