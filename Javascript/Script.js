676c8ab093fb71287e4d6f38f7ad4acdb238b271
Cretrok <solosemeocurioesto@gmail.com>
2019-06-08 13:24:03 -0500

    Correccion de alguno errores, JS innerHTML en h2

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,4 +1,5 @@
 var casillas = document.querySelectorAll("main div");
+const h2 = document.querySelector("h2");
 var turno = "X";
 const reinicio = document.querySelector("reinicio");
 
@@ -6,10 +7,12 @@ casillas.forEach(casilla => {
     casilla.addEventListener( "click", function (evento) {
     	if( turno == 'X'){
     		this.innerHTML = `<span class="movimiento croos">${turno}</span>`;
+    		h2.innerHTML = "Usuario 1";
     		turno = "O";
-    		console.log("click en: "+casilla);
+    		//console.log("click en: "+casilla);
     	}else{
     		this.innerHTML = `<span class="movimiento circle">${turno}</span>`
+    		h2.innerHTML = "Usuario 2";
     		turno = "X";
     	}
     });
